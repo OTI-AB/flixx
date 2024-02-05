11f15821416e012fe21a92776408217d8e535997
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-02-05 06:14:24 -0700

    Added Popular Movies display functionality

--- js/script.js
+++ js/script.js
@@ -2,8 +2,49 @@ const globaState = {
 	currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+	const { results } = await getTMDBData('movie/popular');
+
+	results.forEach((movie) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+		${
+			movie.poster_path
+				? `<img
+			src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+			class="card-img-top"
+			alt="${movie.title}"
+		/>`
+				: `<img
+		src="../images/no-image.jpg"
+		class="card-img-top"
+		alt="${movie.title}"
+	/>`
+		}
+	</a>
+	<div class="card-body">
+		<h5 class="card-title">${movie.title}</h5>
+		<p class="card-text">
+			<small class="text-muted">Release: ${movie.release_date}</small>
+		</p>
+	</div>
+`;
+
+		document.querySelector('#popular-movies').appendChild(div);
+	});
+}
+
 async function getTMDBData(endpoint) {
 	const API_KEY = '894794e35b50d506b0c2602faad70632';
+	const API_URL = 'https://api.themoviedb.org/3/';
+
+	const response = await fetch(
+		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+	);
+
+	const data = await response.json();
+	return data;
 }
 
 function highlightActiveLink() {
@@ -19,7 +60,7 @@ function init() {
 	switch (globaState.currentPage) {
 		case '/':
 		case '/index.html':
-			console.log('Home');
+			displayPopularMovies();
 			break;
 		case '/shows.html':
 			console.log('TV Shows');
