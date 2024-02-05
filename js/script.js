0072974bc2938eb64028ad8566d83df4ffad1872
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-02-05 07:12:47 -0700

    Added TV Shows display functionality

--- js/script.js
+++ js/script.js
@@ -35,6 +35,42 @@ async function displayPopularMovies() {
 	});
 }
 
+async function displayPopularTVShows() {
+	const { results } = await getTMDBData('tv/popular');
+
+	results.forEach((tv) => {
+		console.log(tv);
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `<a href="tv-details.html?id=${tv.id}">
+				${
+					tv.poster_path
+						? `<img
+					src="https://image.tmdb.org/t/p/w500${tv.poster_path}"
+					class="card-img-top"
+					alt="${tv.name}"
+				/>`
+						: `<img
+				src="../images/no-image.jpg"
+				class="card-img-top"
+				alt="${tv.name}"
+			/>`
+				}
+			</a>
+			<div class="card-body">
+				<h5 class="card-name">${tv.name}</h5>
+				<p class="card-text">
+					<small class="text-muted">First Air: ${tv.first_air_date}</small>
+				</p>
+				<p class="card-text">
+					<small class="text-muted">Language: ${tv.original_language}</small>
+				</p>
+			</div>
+		`;
+		document.querySelector('#popular-shows').appendChild(div);
+	});
+}
+
 async function getTMDBData(endpoint) {
 	const API_KEY = '894794e35b50d506b0c2602faad70632';
 	const API_URL = 'https://api.themoviedb.org/3/';
@@ -43,8 +79,21 @@ async function getTMDBData(endpoint) {
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
 	);
 
+	showSpinner();
+
 	const data = await response.json();
+
+	hideSpinner();
+
 	return data;
+}
+
+function showSpinner() {
+	document.querySelector('.spinner').style.display = 'block';
+}
+
+function hideSpinner() {
+	document.querySelector('.spinner').style.display = 'none';
 }
 
 function highlightActiveLink() {
@@ -63,7 +112,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			console.log('TV Shows');
+			displayPopularTVShows();
 			break;
 		case '/movie-details.html':
 			console.log('Movie Details');
