538d0a910c2ff559ae1fa5a8f2e53aca20ce7ba6
OTI-AB <phillip.pearson@onsitetechnologies.caB>
2024-02-06 13:12:40 -0700

    Added movie and tv show details functionality

--- js/script.js
+++ js/script.js
@@ -35,11 +35,140 @@ async function displayPopularMovies() {
 	});
 }
 
+async function displayMovieDetails() {
+	const movie_id = window.location.search.split('=')[1];
+
+	const movie = await getTMDBData(`movie/${movie_id}`);
+
+	addBackdrop('movie', movie.backdrop_path);
+
+	const div = document.createElement('div');
+
+	div.innerHTML = `<div class="details-top">
+	<div>
+	${
+		movie.poster_path
+			? `<img
+		src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+		class="card-img-top"
+		alt="${movie.title}"
+	/>`
+			: `<img
+	src="../images/no-image.jpg"
+	class="card-img-top"
+	alt="${movie.title}"
+/>`
+	} 
+	</div>
+	<div>
+		<h2>${movie.title}</h2>
+		<p>
+			<i class="fas fa-star text-primary"></i>
+			${movie.vote_average.toFixed(1)} / 10
+		</p>
+		<p class="text-muted">Release Date: ${movie.release_date}</p>
+		<p>
+			${movie.overview}
+		</p>
+		<h5>Genres</h5>
+		<ul class="list-group">
+			${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+		</ul>
+		<a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+	</div>
+</div>
+<div class="details-bottom">
+	<h2>Movie Info</h2>
+	<ul>
+		<li><span class="text-secondary">Budget: $</span> ${
+			movie.budget ? `${movie.budget.toLocaleString()}` : `Unavailable`
+		} </li>
+		<li><span class="text-secondary">Revenue: $</span> ${
+			movie.revenue ? `${movie.revenue.toLocaleString()}` : `Unavailable`
+		} </li>
+		<li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+		<li><span class="text-secondary">Status:</span> ${movie.status}</li>
+	</ul>
+	<h4>Production Companies</h4>
+	<div class="list-group">${movie.production_companies
+		.map((company) => `<li>${company.name}</li>`)
+		.join('')}</div>
+</div>
+`;
+	document.getElementById('movie-details').appendChild(div);
+}
+
+async function displayShowDetails() {
+	const show_id = window.location.search.split('=')[1];
+
+	const show = await getTMDBData(`tv/${show_id}`);
+
+	addBackdrop('show', show.backdrop_path);
+
+	const div = document.createElement('div');
+
+	div.innerHTML = `<div class="details-top">
+	<div>${
+		show.poster_path
+			? `<img
+		src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+		class="card-img-top"
+		alt="${show.name}"
+	/>`
+			: `<img
+	src="../images/no-image.jpg"
+	class="card-img-top"
+	alt="${show.name}"
+/>`
+	} 
+	</div>
+	<div>
+		<h2>${show.name}</h2>
+		<p>
+			<i class="fas fa-star text-primary"></i>
+			${show.vote_average.toFixed(1)} / 10
+		</p>
+		<p class="text-muted">Release Date: ${show.first_air_date}</p>
+		<p>
+			${show.overview}
+		</p>
+		<h5>Genres</h5>
+		<ul class="list-group">
+		${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+		</ul>
+		<a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+	</div>
+</div>
+<div class="details-bottom">
+	<h2>Show Info</h2>
+	<ul>
+		<li><span class="text-secondary">Number Of Episodes:</span> ${
+			show.number_of_episodes
+		} over ${show.number_of_seasons} Seasons</li>
+		<li><span class="text-secondary">Languages: ${show.spoken_languages
+			.map((language) => `<li>${language.name}</li>`)
+			.join('')}
+		<li>
+			<span class="text-secondary">Last Episode To Air:</span> ${
+				show.last_episode_to_air.name
+			}
+		</li>
+		<li><span class="text-secondary">Status:</span> ${show.status}</li>
+	</ul>
+	<h4>Production Companies</h4>
+	<div class="list-group">${show.production_companies
+		.map((company) => `<li>${company.name}</li>`)
+		.join('')}</div>
+</div>
+</div>
+`;
+	document.getElementById('show-details').appendChild(div);
+}
+
 async function displayPopularTVShows() {
 	const { results } = await getTMDBData('tv/popular');
 
 	results.forEach((tv) => {
-		console.log(tv);
 		const div = document.createElement('div');
 		div.classList.add('card');
 		div.innerHTML = `<a href="tv-details.html?id=${tv.id}">
@@ -69,6 +198,27 @@ async function displayPopularTVShows() {
 		`;
 		document.querySelector('#popular-shows').appendChild(div);
 	});
+}
+
+function addBackdrop(type, backgoundPath) {
+	const overlayDiv = document.createElement('div');
+	overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgoundPath})`;
+	overlayDiv.style.backgroundSize = 'cover';
+	overlayDiv.style.backgroundPosition = 'center';
+	overlayDiv.style.backgroundRepeat = 'no-repeat';
+	overlayDiv.style.height = '100vh';
+	overlayDiv.style.width = '100vw';
+	overlayDiv.style.position = 'absolute';
+	overlayDiv.style.top = '0';
+	overlayDiv.style.left = '0';
+	overlayDiv.style.zIndex = '-1';
+	overlayDiv.style.opacity = '0.2';
+
+	if (type === 'movie') {
+		document.querySelector('#movie-details').appendChild(overlayDiv);
+	} else {
+		document.querySelector('#show-details').appendChild(overlayDiv);
+	}
 }
 
 async function getTMDBData(endpoint) {
@@ -115,10 +265,10 @@ function init() {
 			displayPopularTVShows();
 			break;
 		case '/movie-details.html':
-			console.log('Movie Details');
+			displayMovieDetails();
 			break;
 		case '/tv-details.html':
-			console.log('Tv Details');
+			displayShowDetails();
 			break;
 		case '/search.html':
 			console.log('Search Page');
